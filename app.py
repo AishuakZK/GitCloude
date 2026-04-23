8c9dba7f4092e17d35eec7e47392af99e52a9657
Aish <aishuak05zk@gmail.com>
2026-04-23 15:28:39 +0500

    fix tests

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-def add(a, b):
-    return a + b
+import math
 
+def uuu(n):
+    return math.sqrt(n) % 1 == 0
+
+print(uuu(9))   
+print(uuu(8))    
+
+
+<<<<<<< HEAD
 if __name__ == "__main__":
     print("Result:", add(2, 3))
 TEST
+=======
+>>>>>>> 5b20531 (fix tests)
--- test_app.py
+++ test_app.py
@@ -1,5 +1,10 @@
-from app import add
+from app import uuu
 
+<<<<<<< HEAD
 def test_add():
     assert add(2, 3) == 5
 # new line
+=======
+def test_uuu_true():
+    assert uuu(9) == True
+>>>>>>> 5b20531 (fix tests)
