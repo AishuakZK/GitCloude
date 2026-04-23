c58cdd2001c1a6055ec6d7202bd933af9eeeae78
Aish <aishuak05zk@gmail.com>
2026-04-23 15:40:13 +0500

    fix tests

--- app.py
+++ app.py
@@ -7,9 +7,3 @@ print(uuu(9))
 print(uuu(8))    
 
 
-<<<<<<< HEAD
-if __name__ == "__main__":
-    print("Result:", add(2, 3))
-TEST
-=======
->>>>>>> 5b20531 (fix tests)
--- test_app.py
+++ test_app.py
@@ -1,10 +1,7 @@
 from app import uuu
 
-<<<<<<< HEAD
-def test_add():
-    assert add(2, 3) == 5
-# new line
-=======
 def test_uuu_true():
     assert uuu(9) == True
->>>>>>> 5b20531 (fix tests)
+
+def test_uuu_false():
+    assert uuu(8) is False
